10478c5bb16fcc7aa9fa8e77689f39eeae5f5f29
lehieuphuoc <lehieuphuoc35205@gmail.com>
2025-10-17 13:55:26 +0700

    Setup môi trường 2

---
+++ fe-cms/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>
+      trang chu
+      <div>
+        <Button type="primary">Antd Button</Button>
+      </div>
+    </div>
+  );
+}
